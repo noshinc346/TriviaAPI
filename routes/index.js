a1d66c98b276e81a23dc97b247149424d301ae15
Noshinc346 <noshin.chowdhury31@gmail.com>
2024-03-06 12:47:09 -0500

    index route file done

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import questionRoutes from "./questions.js"
+
+const router = Router();
+
+router.get("/", (req, res) => res.send("This is the api root!"));
+
+router.use("/questions", questionRoutes)
+
+export default router;
