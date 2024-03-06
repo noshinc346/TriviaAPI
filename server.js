f55af7765d8931bf4b5aed4be72d3c7de0e89415
Noshinc346 <noshin.chowdhury31@gmail.com>
2024-03-06 11:57:54 -0500

    server js done - need route file

--- server.js
+++ server.js
@@ -4,3 +4,21 @@ import express from "express";
 import cors from "cors";
 import logger from "morgan";
 import chalk from "chalk";
+
+const app = express();
+const PORT = 3030;
+
+app.use(express.json());
+app.use(cors());
+app.use(logger("dev"));
+
+app.use("/api", routes);
+
+db.on("connected", () => {
+  console.clear();
+  console.log(chalk.blue("Connected to MongoDB!"));
+
+  app.listen(PORT, () => {
+    console.log(chalk.magenta(`Express server running on port ${PORT}`));
+  });
+});
