37f8411c4757231f54f7d13b5f5e9400baa0e1a6
Noshinc346 <noshin.chowdhury31@gmail.com>
2024-03-06 11:54:28 -0500

    model created and server file made

---
+++ models/Question.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+let QuestionSchema = new Schema({
+    "type": { type: String },
+    "difficulty": { 
+                type: String,
+                 enum: ["easy, medium, hard"]   
+                },
+    "category": { type: String },
+    "question": { type: String },
+    "correct_answer": { type: String },
+    "incorrect_answers": [ { type: String } ]
+})
+
+export default mongoose.model("questions", QuestionSchema)
---
+++ server.js
@@ -0,0 +1,6 @@
+import db from "./db/connection.js";
+// import routes 
+import express from "express";
+import cors from "cors";
+import logger from "morgan";
+import chalk from "chalk";
